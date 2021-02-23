2da24a98a6d74bd0935f133f5f159d5ea13a7a67
José Jardel Luciano Ferreira da Silva <32444652+housekore@users.noreply.github.com>
2021-02-23 17:44:54 -0300

    Create donate.py

---
+++ donate.py
@@ -0,0 +1,7 @@
+## Donate
+I am currently unemployed. If you are interested in making a donation via pix, you can use CPF 05966998671. In the name of José Jardel
+
+No momento estou desempregado. Caso seja do seu interesse realizar uma doação via pix, pode utilizar o CPF 05966998671. Em nome de José Jardel
+
+#Compartilhe
+Por favor, ajude a compartilhar a ferramenta com mais profissionais da área.
